7aef261ac9788ff60f1bd92e91863929e880c98e
arrianne <arrianneoshea@hotmail.co.uk>
2021-05-14 15:47:13 +1000

    writing my first jsx and formatting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,11 +4,40 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'This is JSX'
+  React.createElement(
+    'h1',
+    null,
+    'This is JSX'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  )
+);
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Arrianne'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age : 33'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Sunshine Coast'
+  )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -3,8 +3,21 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = <p>This is JSX</p>
+var template = (
+  <div>
+    <h1>This is JSX</h1>
+    <p>This is some info</p>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Arrianne</h1>
+    <p>Age : 33</p>
+    <p>Sunshine Coast</p>
+  </div>
+);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
